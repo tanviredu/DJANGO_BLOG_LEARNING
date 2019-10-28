06e005d62a3989c3c9e3c1f99ddb419dae05cfc0
mechanic <mechanic@localhost.localdomain>
2019-10-28 04:48:18 -0400

    adding view

--- blogproject/blogapp/views.py
+++ blogproject/blogapp/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+##creating a simple view
+## request have to be the parameter
+def hello(request):
+    text = "<h1> hello world </h2>"
+    return HttpResponse(text)
 
-# Create your views here.
+def hellorender(request):
+    return render('hello.html')
+
+def hello_with_param(request,username):
+    return HttpResponse('your username is '%username)
