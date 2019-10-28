e26adda4a92a1187e78fe068b32618aa2881c79b
mechanic <mechanic@localhost.localdomain>
2019-10-28 05:43:00 -0400

    added

--- blogproject/blogapp/urls.py
+++ blogproject/blogapp/urls.py
@@ -13,10 +13,11 @@ from . import views
 #3 inclide the views
 
 urlpatterns = [
-    path('hello/',views.hello,name='hello'),
-    path('hellorender/',views.hellorender,name='hellorender'),
-    path('hello_with_param/<str:username>',views.hello_with_param,name='hello_with_param'),
-    path('hello_with_multiple_param/<str:id>/<str:name>',views.hello_with_multiple_param,name='hello_with_multiple_param'),
-    path('hello_again/<str:id>/<str:name>/<str:department>',views.hello_again,name='hello_again'),
+    #path('hello/',views.hello,name='hello'),
+    #path('hellorender/',views.hellorender,name='hellorender'),
+    #path('hello_with_param/<str:username>',views.hello_with_param,name='hello_with_param'),
+    #path('hello_with_multiple_param/<str:id>/<str:name>',views.hello_with_multiple_param,name='hello_with_multiple_param'),
+    #path('hello_again/<str:id>/<str:name>/<str:department>',views.hello_again,name='hello_again'),
+    path('render_index/',views.render_index,name='render_index')
     
 ]
--- blogproject/blogapp/views.py
+++ blogproject/blogapp/views.py
@@ -2,20 +2,32 @@ from django.shortcuts import render
 from django.http import HttpResponse
 ##creating a simple view
 ## request have to be the parameter
-def hello(request):
-    text = "<h1> hello world </h2>"
-    return HttpResponse(text)
+#def hello(request):
+#    text = "<h1> hello world </h2>"
+#    return HttpResponse(text)
 
-def hellorender(request):
-    return render(request,'hello.html')
+#def hellorender(request):
+#    return render(request,'hello.html')
 
-def hello_with_param(request,username):
-    return HttpResponse('your username is '+str(username))
+#def hello_with_param(request,username):
+#    return HttpResponse('your username is '+str(username))
 
-def hello_with_multiple_param(request,id=None,name=None):
-    text = "id "+str(id)+" "+"name "+str(name)
-    return HttpResponse(text)
+#def hello_with_multiple_param(request,id=None,name=None):
+#    text = "id "+str(id)+" "+"name "+str(name)
+#    return HttpResponse(text)
 
-def hello_again(request,id=None,name=None,department=None):
-    text = "id "+str(id)+" "+"name "+str(name)+" "+"Department "+str(department)
-    return HttpResponse(text)
+#def hello_again(request,id=None,name=None,department=None):
+#    text = "id "+str(id)+" "+"name "+str(name)+" "+"Department "+str(department)
+#    return HttpResponse(text)
+
+## now from this we render the html page
+# the page have to be in the template folder
+def render_index(request):
+    id = 143000410
+    name = "Tanvir Rahman"
+    department = "EEE"
+    
+    ## you have to send  adictionary to
+    context = {'id':id,'name':name,'department':department}
+    #return render(request,'first.html',context)
+    return render(request,"public/first.html",context)
